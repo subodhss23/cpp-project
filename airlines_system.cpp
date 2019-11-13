dc411045434f3b282452ab830e4e73f4fcf9d560
subodhss23 <subodhss23@gmail.com>
2019-11-12 23:52:23 -0800

    boilerplate

---
+++ airlines_system.cpp
@@ -0,0 +1,24 @@
+
+// airlines management system
+
+#include <iostream>
+using namespace std;
+
+
+int main(void){
+	
+	cout << "1. Airlines Reservation System Menu" << '\n';
+	cout << "2. Reserve seat" << '\n';
+	cout << "3. User ticket" << '\n';
+	cout << "4. Flights schedule" << '\n';
+	cout << "5. Display Passanger" << '\n';
+	cout << "6. Exit program" << '\n';
+
+	int i;
+	cout << "Choose One: ";
+	cin >> i;
+	cout << "\n" << "You have choose " << i << "\n";
+
+	return 0;
+
+}
