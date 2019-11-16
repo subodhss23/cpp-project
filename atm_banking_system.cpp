1c4a29f9d128ff6a3530931d256cb537e155849a
subodhss23 <subodhss23@gmail.com>
2019-11-15 20:27:35 -0800

    boiler plate for atm_system

---
+++ atm_banking_system.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int pin_collection[] ={100,200,300, 400};
+	int pin_code;
+	int size = sizeof(pin_collection/pin_collection[0]);
+
+	cout << size << "\n";
+	cout << "****************" << "\n";
+		
+	cout << "Thanks for Entering your card " << "\n";
+	cout << " Enter your pin code,now: - ";
+	
