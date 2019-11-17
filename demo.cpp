e261edc587089a05772d94ddd92cf978cad2eeca
subodhss23 <subodhss23@gmail.com>
2019-11-17 07:30:26 -0800

    get it right

--- demo.cpp
+++ demo.cpp
@@ -1,23 +1,41 @@
 #include<iostream>
 using namespace std;
 
-class Newclass{
-	public:
-	void print(){
-		cout << "What is up??" << "\n";
-	}
+class Employee{
+	private:
+		int salary;
 
-	void printing_again(int x);
+	public:
+		void setSalary(int x){
+			salary = x;
+		}
+
+		int getSalary(){
+			return salary;
+		}
 };
 
-void Newclass::printing_again(int x){
-	cout << "We are printing again " << x << "\n";
-}
+class Vechicle{
+	private:
+		int year;
+
+	public:
+		void setyear(int x){
+			year = x;
+		}
+
+		int getyear(){
+			return year;
+		}
+};
 
 int main(){
-	Newclass newobj;
+	// Employee newobj;
+	// newobj.setSalary(1000);
+	// cout << newobj.getSalary() ;
 
-	newobj.print();
-	newobj.printing_again(5);
+	Vechicle newobj1;
+	newobj1.setyear(1992);
+	newobj1.getyear();
 	return 0;
 }
