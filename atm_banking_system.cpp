167a486e49ea9960b52159d82147c9e71bd8a550
subodhss23 <subodhss23@gmail.com>
2019-11-18 11:23:21 -0800

    debugging

--- atm_banking_system.cpp
+++ atm_banking_system.cpp
@@ -15,13 +15,33 @@ class ATM{
 		cout << "You have enough balance in your account "<< "\n";
 	}
 
-	void business(int i){
-		cout << i << endl;
+	
+	
+	int  business(int i){
 		cout << "Choose option from the menu:- " <<"\n";
+	
+		cout << "1.Check your balance. " << "\n";
+		cout << "2.Withdraw cash from checking account. " << "\n";
+		cout << "3.Withdram cash from saving account. " << "\n";
 		cin >> i;
-		
-		cout << "you have choose " << i << "\n";
+		cout << "you have choose " << i << "\n";	
+	
+		return i;
 	}
+
+	void options(){
+		int business(int i);
+		if(i == 1){
+			cout << "you have choose option one" << "\n";
+		} else if (i == 2){
+			cout << "you are now into your checking account" << "\n";
+		} else if (i == 3){
+			cout << "you are now in your saving account " << "\n";
+		} else {
+			cout << "Not a valid option" << endl; 
+		}
+	}
+	
 
 };
 
@@ -43,7 +63,8 @@ int main(){
 		if(pin_collection[i] == newobj.pin){
 			cout<<"Welcome in our valuable customer.."<< "\n";
 			
-			newobj.business(newobj.pin);
+			//newobj.business(newobj.pin);
+			newobj.options();
 			return 0;
 		}
 	}
