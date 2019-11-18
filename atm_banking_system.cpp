cfc10141532cacd38f7ff24214fb58b0abc8a1a6
subodhss23 <subodhss23@gmail.com>
2019-11-18 11:01:16 -0800

    building object on one single class

--- atm_banking_system.cpp
+++ atm_banking_system.cpp
@@ -1,30 +1,53 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
+
+
+class ATM{
+	public:
+
+	ATM(){
+		int pin_collection[] = {100,200,300,400};
+	}
+
+	int pin;
+	
+	void check_balance(){
+		cout << "You have enough balance in your account "<< "\n";
+	}
+
+	void business(int i){
+		cout << i << endl;
+		cout << "Choose option from the menu:- " <<"\n";
+		cin >> i;
+		
+		cout << "you have choose " << i << "\n";
+	}
+
+};
+
+
 
 int main(){
 	
-	int pin_collection[] ={100,200,300, 400};
-	int pin_code;
+	int pin_collection[] = {100,200,300};	
 	int size = sizeof(pin_collection)/sizeof(pin_collection[0]);
-	bool found = false;
-		
-	cout << "Thanks for Entering your card " << "\n";
-	cout << " Enter your pin code,now: - ";
-	cin >> pin_code;
+
+	cout << "*****Welcome to the Bank of West portal********" << "\n";
+	cout << "Enter you pin code:- ";
+	
+	ATM newobj;
+	
+	cin >> newobj.pin;
 
 	for (int i = 0; i < size; i++){
-		if(pin_code == pin_collection[0]){
-			cout << "Welcome in" << "\n";
-			found = true;
-			break;
+		if(pin_collection[i] == newobj.pin){
+			cout<<"Welcome in our valuable customer.."<< "\n";
+			
+			newobj.business(newobj.pin);
+			return 0;
 		}
 	}
-
-	if(found){
-		cout << "found" << endl;
-	} else {
-		cout << "not found" << endl;
-	}
+	cout << "you pin does not match this time" << "\n";
 	return 0;
-
-}	
+}
+	
