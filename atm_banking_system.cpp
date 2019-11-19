565e45c2cc961c1476fba997fefbccfd0b1b28be
subodhss23 <subodhss23@gmail.com>
2019-11-18 21:20:45 -0800

    atm options

--- atm_banking_system.cpp
+++ atm_banking_system.cpp
@@ -4,71 +4,69 @@ using namespace std;
 
 class ATM{
 	public:
-
-	ATM(){
-		int pin_collection[] = {100,200,300,400};
-	}
-
-	int pin;
 	
-	void check_balance(){
-		cout << "You have enough balance in your account "<< "\n";
-	}
-
-	
-	
-	int  business(int i){
-		cout << "Choose option from the menu:- " <<"\n";
-	
-		cout << "1.Check your balance. " << "\n";
-		cout << "2.Withdraw cash from checking account. " << "\n";
-		cout << "3.Withdram cash from saving account. " << "\n";
-		cin >> i;
-		cout << "you have choose " << i << "\n";	
-	
-		return i;
-	}
-
-	void options(){
-		int business(int i);
-		if(i == 1){
-			cout << "you have choose option one" << "\n";
-		} else if (i == 2){
-			cout << "you are now into your checking account" << "\n";
-		} else if (i == 3){
-			cout << "you are now in your saving account " << "\n";
-		} else {
-			cout << "Not a valid option" << endl; 
+	// check if the pin is in the system
+	int check_pin(int x){
+	int pin_collection[] = {100,200,300,400};
+		for (int i = 0; i < 4; i++){
+			if(x == pin_collection[i]){
+				cout << "pin found"<< endl;
+				return x;
+			}
 		}
+		cout << "value not found" << endl;
+		return 0;
 	}
-	
+
+	// choose menu option
+	void options(){
+		cout << "Choose an option from below  " << "\n";
+		cout << "1. Check balance. " << "\n";
+		cout << "2. View balance in checking account. " << "\n";
+		cout << "3. View balance in saving account. " << "\n";
+		
+	}
+
+	// validate option
+	void validation_option(int n){
+		if(n == 1){
+			cout << "Your balance is $40 " << "\n";
+		} else if (n == 2){
+			cout << "Your balance in checking account is $100" << "\n";
+		} else if (n == 3){
+			cout << "your balance in saving account is $0" << "\n";
+		} else {
+			cout << "not a valid option" << "\n";
+		}
+	}	
 
 };
 
 
-
 int main(){
+	system("clear");
+	cout << "Enter your pin number below:- " << "\n";
 	
-	int pin_collection[] = {100,200,300};	
-	int size = sizeof(pin_collection)/sizeof(pin_collection[0]);
-
-	cout << "*****Welcome to the Bank of West portal********" << "\n";
-	cout << "Enter you pin code:- ";
+	int pin;
+	cin >> pin;
 	
 	ATM newobj;
 	
-	cin >> newobj.pin;
+	newobj.check_pin(pin);
 
-	for (int i = 0; i < size; i++){
-		if(pin_collection[i] == newobj.pin){
-			cout<<"Welcome in our valuable customer.."<< "\n";
-			
-			//newobj.business(newobj.pin);
-			newobj.options();
-			return 0;
-		}
-	}
-	cout << "you pin does not match this time" << "\n";
+	system("clear");
+	newobj.options();
+
+	int n;
+	cin >> n;
+	system("clear");
+
+	newobj.validation_option(n);
+	
+	
+	
 	return 0;
+
 }
+
 	
