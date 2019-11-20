ed90175a2606ecb6f66b96a293c1a99ad0d3bfc9
subodhss23 <subodhss23@gmail.com>
2019-11-19 21:53:46 -0800

    inline

--- demo.cpp
+++ demo.cpp
@@ -1,15 +1,25 @@
 #include<iostream>
 using namespace std;
 
+typedef unsigned short int USHORT;
+
+
+inline void add(){
+	USHORT i = 10;
+	USHORT j = 20;
+	
+	USHORT addi = 0;
+
+	addi = i + j;
+
+	cout << addi << endl;
+
+}
+
 
 int main(){
-
-	int i = 10;
-	int *j = &i;
-
-
-	cout << j << " This is the address of i" << "\n";
-	cout << &i << " &i" ;
+	inline void add();
 
 	return 0;
 }
+	
