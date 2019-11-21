a584f0e198545e93c0394dc0e2afe76a1ba614ce
subodhss23 <subodhss23@gmail.com>
2019-11-20 20:10:06 -0800

    iterating vector

--- stl.cpp
+++ stl.cpp
@@ -4,6 +4,10 @@
  
 using namespace std;
 
+bool f(int x, int y){
+	 return x > y;
+}
+
 int main(){
 	
 	vector<int> A = {10,9,8,1,2,3,4,5};
@@ -19,14 +23,70 @@ int main(){
 	A.push_back(100);
 	bool present_2 = binary_search(A.begin(), A.end(), 100);
 
-	cout << present_2;
+	cout << present_2 << endl;
 	
 	A.push_back(100);
 	A.push_back(100);
 	A.push_back(100);
 	A.push_back(123);
-
+ 
 	vector<int>::iterator it = lower_bound(A.begin(), A.end(),100);
+	vector<int>::iterator it1 = upper_bound(A.begin(), A.end(),100);
+
+	cout << *it << endl;
+	cout << *it1 << endl;
+	
+	cout << it1 - it << endl;
+	cout << endl;
+	sort(A.begin(), A.end(),f);
+	vector<int>::iterator it3;
+	
+	for (it3 = A.begin(); it3 != A.end(); it3++){
+		cout << *it3 << endl;
+	}
+
+// *** Summary of above experiment *** //
+// 	sort vector of integers
+//	binary search to check if the element is present
+//	if vector is sorted -> upper_bound and lower_bound can be done
+//	finding the duplication by subtracting upper_bound to lower_bound
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	return 0;
 	
 }
